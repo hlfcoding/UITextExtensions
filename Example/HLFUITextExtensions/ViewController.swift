25e7109292414b1275fe26602ef38b5cb491af95
Peng Wang <peng@pengxwang.com>
2017-06-11 21:59:02 -0700

    Add example source, initial example app

--- Example/HLFUITextExtensions/ViewController.swift
+++ Example/HLFUITextExtensions/ViewController.swift
@@ -7,18 +7,30 @@
 //
 
 import UIKit
+import HLFUITextExtensions
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setUpLabelExample()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func setUpLabelExample() {
+        label.backgroundColor = .clear
+        label.font = .preferredFont(forTextStyle: .title1)
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.textColor = .darkText
+        label.hlf_updateText("Hello, playground. You are looking okay.")
+
+        label.hlf_setKerning(-0.5)
+        label.hlf_setLineHeight(label.hlf_pointsByEms(1.1))
+        label.hlf_updateText("Hello, playground. You are looking good.")
+
+        label.sizeToFit()
     }
 
 }
-
